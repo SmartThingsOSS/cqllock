82b9f0237f7f59c05680322fa2eaf3a05b2e89b7
Adam Mielke <adam.mielke@smartthings.com>
2015-10-06 13:18:55 -0500

    add debug logging for cql session parameters

--- session.go
+++ session.go
@@ -11,6 +11,7 @@ func (config *Config) session() *gocql.Session {
 	cluster := gocql.NewCluster(config.Seeds...)
 
 	if config.CertPath != "" {
+		log.Debugf("using SSL with certificate '%s' and key '%s'", config.CertPath, config.KeyPath)
 		cluster.SslOpts = &gocql.SslOptions{
 			CertPath:               expandHome(config.CertPath),
 			KeyPath:                expandHome(config.KeyPath),
@@ -19,6 +20,7 @@ func (config *Config) session() *gocql.Session {
 	}
 
 	if config.Username != "" {
+		log.Debugf("using authentication with username '%s'", config.Username)
 		cluster.Authenticator = gocql.PasswordAuthenticator{
 			Username: config.Username,
 			Password: config.Password,
@@ -26,10 +28,12 @@ func (config *Config) session() *gocql.Session {
 	}
 
 	if config.Timeout > 0 {
-		cluster.Timeout = time.Second * config.Timeout
+		cluster.Timeout = time.Second * time.Duration(config.Timeout)
+		log.Debugf("setting cluster timeout to %v", cluster.Timeout)
 	}
 
 	if config.Retries > 0 {
+		log.Debugf("setting cluster retry number to %d", config.Retries)
 		cluster.RetryPolicy = &gocql.SimpleRetryPolicy{
 			NumRetries: config.Retries,
 		}
